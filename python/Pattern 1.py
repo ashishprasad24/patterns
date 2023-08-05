a9bc690d814271ebdc56b2a00144aeb94f66c76c
ashishprasad24 <86977006+ashishprasad24@users.noreply.github.com>
2023-08-05 17:15:42 +0530

    Create Pattern 1.py

---
+++ python/Pattern 1.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def printSquare(self, N):
+        # Code here
+        for i in range (N):
+            for j in range(N):
+                print("* ",end="")
+            print()    
+                    
+            
+
