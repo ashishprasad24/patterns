b7bdd8f1493f21d6f04668757c1526b0bd3eb613
ashishprasad24 <86977006+ashishprasad24@users.noreply.github.com>
2023-08-06 12:55:48 +0530

    Create pattern5.cpp

---
+++ c++/pattern5.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	    // code here
+	    for(int i=1;i<=n;i++ ){
+	        for (int j=1;j<=n-i+1;j++){
+	            cout<<"* ";
+	        }
+	        cout<<endl; 
+	    }
+	   
+	}
+};
